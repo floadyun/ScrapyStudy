bd903d77b83290ae6e90db2348bb5b6849bac08f
floadyun <floadyun@163.com>
2017-11-15 15:06:59 +0800

    新增爬取豆瓣250爬虫

--- ScrapyStudy/pipelines.py
+++ ScrapyStudy/pipelines.py
@@ -8,7 +8,7 @@ class XHPipeline(object):
 
     def process_item(self, item, spider):
         # 读取item中的数据
-        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        line = json.dumps(dict(item), ensure_ascii=False) + ",\n"
         # 写入文件
         self.file.write(line)
         # 返回item
